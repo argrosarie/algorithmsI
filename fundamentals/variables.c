da642cc3f2f53a797a7e84d6b825d796244e1a6d
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-22 09:05:40 -0300

    First exercise

---
+++ fundamentals/variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main () {
+    int firstNumber = 25;
+    int secondNumber = 13;
+
+    printf("El primer número es %i\n", firstNumber);
+    printf("El segundo número es %i\n", secondNumber);
+
+    return 0;
+}
